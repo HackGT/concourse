31b998e9dcf27191c89a6d45d4bd9792e63b4243
Michael Eden <themichaeleden@gmail.com>
2017-01-20 10:20:41 -0500

    added template for meta pipeline

---
+++ tasks/generate-meta-pipelines.rb
@@ -0,0 +1,51 @@
+require 'yaml'
+require 'erb'
+require_relative './lib.rb'
+
+# parameters
+team      = get_env 'PIPEDREAM_TEAM'
+out_dir   = get_env 'PIPEDREAM_OUT_DIR'
+out_file  = get_env 'PIPEDREAM_OUT_FILE'
+biodomes  = get_env 'PIPEDREAM_BIODOMES_DIR'
+
+# constants
+SOURCE_DIR = File.expand_path(File.dirname(__FILE__))
+BASE_PIPE = File.join SOURCE_DIR, '../pipelines/meta.yaml'
+META_TEMPLATE = File.join SOURCE_DIR, '../templates/meta-pipeline.yaml.erb'
+pipeline_path = File.join out_dir, '0001-gen-pipeline.yaml'
+secrets_file = File.join out_dir, 'secrets.yaml'
+
+# collect all the secrets
+secrets = collect_prefixes ENV, 'secret_'
+
+# build the new meta pipeline
+meta_pipeline = Dir[File.join biodomes, '*']
+  .map do |dome_file|
+    biodome = YAML.load (File.read dome_file)
+    biodome[:biodome] = basename_no_ext dome_file
+    biodome[:secrets] = secrets.keys
+    YAML.load (ERB.new(File.read META_TEMPLATE).result(binding))
+  end
+  .reduce(load_yaml (File.read BASE_PIPE)) { |memo, aug| merge_pipelines memo, aug }
+
+# pipelines description
+pipelines_desc = {
+  'name' => 'meta',
+  'team' => team,
+  'config_file' => pipeline_path,
+  'vars_files' => [secrets_file],
+}
+
+# write everything to disk
+[
+  [pipeline_path, meta_pipeline],
+  [secrets_file, secrets],
+  [out_file, pipelines_desc],
+]
+.each do |path, data|
+  dumped = dump_yaml data
+  File.write path, dumped
+  puts
+  puts path
+  puts dumped
+end
